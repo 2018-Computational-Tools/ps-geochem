546fcc35c40e428f1f4a7c7be09a022b675d781b
Sebastian Kopf <sebastian.kopf@colorado.edu>
2019-01-19 16:21:31 -0700

    docker test

--- binder/install.R
+++
@@ -1,8 +0,0 @@
-
-install.packages("tidyverse")
-
-install.packages("plotly") # interactive plots
-
-install.packages("CHNOSZ") # stability diagrams
-
-install.packages("latex2exp") # complex math on figures
