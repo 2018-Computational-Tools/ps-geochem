a6ac21ceaf435e6e1aa229e294d5809490e18dde
Sebastian Kopf <sebkopf>
2020-09-01 15:53:29 -0600

    binder setup

---
+++ binder/install.R
@@ -0,0 +1,6 @@
+# Regular packages
+install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
+install.packages("tidyverse") # dplyr and ggplot
+install.packages("plotly") # interactive plots
+install.packages("CHNOSZ") # stability diagrams
+install.packages("latex2exp") # complex math on figures
